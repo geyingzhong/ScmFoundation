9a3f8d2f5709ede7c7d02e61b5483ec568fdf1b2
戈应中 <yingzhong_ge@loongjoy.com>
2016-08-28 15:01:10 +0800

    first commit

---
+++ src/scmfoundationServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace geyingzhong\scmfoundation;
+
+use Illuminate\Support\ServiceProvider;
+
+class scmfoundationServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
